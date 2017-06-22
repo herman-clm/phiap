4cc3cbe25d5f706742012fdbcf9bbf820c3eefe8
Selah Lynch <selah@upenn.edu>
2017-06-22 12:50:49 -0400

    More compiling and collapsing data to patient level

--- code/Python/collapse_regex.py
+++ code/Python/collapse_regex.py
@@ -12,7 +12,6 @@ dfg = df.groupby('MRN_deid').sum()[['word_count','regex_matches']]
 dfg['regex_ratio'] = dfg.regex_matches/dfg.word_count
 
 d = pd.to_datetime('today').strftime("%Y%m%d")
-
 dfg.to_csv(proj_dir + "data/collapsed_regex_{}_deid.csv".format(d))
    
 
--- code/Python/patient_level_data.py
+++ code/Python/patient_level_data.py
@@ -15,13 +15,25 @@ df_regex = pd.read_csv(regexfile)
 ##### MEDICATIONS #########
 
 
+#TODO - discuss how to handle Nan's
+
+
 dfmed = pd.read_csv(proj_dir + "data/2_meds.csv")
-dfumed = pd.read_excel(proj_dir + "data/UNIQUE_HYPERTENSION_MEDICATIONS.xlsx")
-sgnlist = dfumed.SIMPLE_GENERIC_NAME.unique().tolist()
+dfhypmed = pd.read_excel(proj_dir + "data/UNIQUE_HYPERTENSION_MEDICATIONS.xlsx")
+sgnlist = dfhypmed.SIMPLE_GENERIC_NAME.unique().tolist()
 
 dfmed['is_hyp_med'] = dfmed.SIMPLE_GENERIC_NAME.isin(sgnlist) & (dfmed.GENERIC_NAME.isnull()==False) & (dfmed.SIMPLE_GENERIC_NAME.isnull()==False)
 dfmedg = dfmed.groupby('MRN_HUP').agg({'is_hyp_med':['sum','count']})
 dfmedg.columns = ['hyp_meds_count', 'all_meds_count']
-dfmedg['hyp_meds_ratio'] = dfmedg.hyp_meds_count/dfmedg.all_meds_count
+      
+dfmedu = dfmed[['MRN_HUP','SIMPLE_GENERIC_NAME','is_hyp_med']].drop_duplicates()
+dfmedug = dfmedu.groupby('MRN_HUP').agg({'is_hyp_med':['sum','count']})    
+dfmedug.columns = ['hyp_meds_uniq_count', 'all_meds_uniq_count']
 
-#TODO - unique meds for a patient
+dfmedcounts = dfmedg.merge(dfmedug, left_index=True, right_index=True)
+d = pd.to_datetime('today').strftime("%Y%m%d")
+dfmedcounts.to_csv(proj_dir + "data/patient_level_data_{}_deid.csv".format(d))
+
+
+#General purpose collapsing on multilevel column names resulting from agg
+#dfmedg.columns = [' '.join(col).strip() for col in dfmedg.columns.values]
