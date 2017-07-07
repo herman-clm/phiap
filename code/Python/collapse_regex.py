86a770a2afb6aea86a2df99c584defb8d1fa7c1b
Selah Lynch <selah@upenn.edu>
2017-07-07 10:38:10 -0400

    Uploading vanderbilt concept identifier and KNIME hypertension module

--- code/Python/collapse_regex.py
+++ code/Python/collapse_regex.py
@@ -1,9 +1,10 @@
 import pandas as pd
 #import numpy as np
 
-proj_dir = '/home/selah/Projects/Daniel_Herman_Aldosterone_2017_03/'
+#proj_dir = '/home/selah/Projects/Daniel_Herman_Aldosterone_2017_03/'
+import proj
 
-notesdatafileregex = proj_dir + "data/regex_notes_20170620.csv"
+notesdatafileregex = proj.dir + "data/regex_notes_20170620.csv"
 df = pd.read_csv(notesdatafileregex)
 df = df[df.Note_Text.isnull() == False]
 df['word_count'] = df.Note_Text.apply(lambda s: len(s.split()))
@@ -16,7 +17,7 @@ def get():
    
 if __name__ == "__main__":
    d = pd.to_datetime('today').strftime("%Y%m%d")
-   dfg.to_csv(proj_dir + "data/collapsed_regex_{}_deid.csv".format(d))
+   dfg.to_csv(proj.dir + "data/collapsed_regex_{}_deid.csv".format(d))
    
 
 
